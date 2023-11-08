721fdc19b8132705fa5090a98320d757f63c5387
George Niculae <george.niculae20@yahoo.com>
2023-11-09 00:29:32 +0200

    Update

---
+++ car-rental-email-notification/src/main/java/com/carrental/emailnotification/service/UserNotificationService.java
@@ -0,0 +1,17 @@
+package com.carrental.emailnotification.service;
+
+import com.carrental.dto.InvoiceDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserNotificationService {
+
+    private final EmailService emailService;
+
+    public void notifyCustomer(InvoiceDto invoiceDto) {
+        emailService.sendEmail(emailService.createMail(invoiceDto.getCustomerEmail(), invoiceDto));
+    }
+
+}
