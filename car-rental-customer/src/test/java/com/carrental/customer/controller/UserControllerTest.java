0c1a72310d09a90f296914e41bf6f25f32d58efa
George Niculae <george.niculae20@yahoo.com>
2023-11-09 23:29:49 +0200

    Update

--- car-rental-customer/src/test/java/com/carrental/customer/controller/UserControllerTest.java
+++ car-rental-customer/src/test/java/com/carrental/customer/controller/UserControllerTest.java
@@ -14,6 +14,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -200,6 +201,7 @@ class UserControllerTest {
     }
 
     @Test
+    @WithAnonymousUser
     void updateUserTest_unauthorized() throws Exception {
         UserDto userDto = TestUtils.getResourceAsJson("/data/UserDto.json", UserDto.class);
 
