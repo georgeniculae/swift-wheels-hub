e9702afcd27daa62709c343a737ba79791589a3c
George Niculae <george.niculae20@yahoo.com>
2023-12-23 22:41:11 +0200

    Update

--- car-rental-api-gateway/car-rental-cloud-gateway/src/main/java/com/carrental/cloudgateway/security/NimbusReactiveJwtDecoder.java
+++ car-rental-api-gateway/car-rental-cloud-gateway/src/main/java/com/carrental/cloudgateway/security/NimbusReactiveJwtDecoder.java
@@ -3,17 +3,16 @@ package com.carrental.cloudgateway.security;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
 
 @Configuration
-public class NimbusJwtDecoderConfig {
+public class NimbusReactiveJwtDecoder {
 
     @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
     private String jwkUri;
 
     @Bean
-    public NimbusReactiveJwtDecoder nimbusReactiveJwtDecoder() {
-        return NimbusReactiveJwtDecoder.withJwkSetUri(jwkUri).build();
+    public org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder nimbusReactiveJwtDecoder() {
+        return org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder.withJwkSetUri(jwkUri).build();
     }
 
 }
