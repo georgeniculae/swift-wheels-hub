527ea3bab5ab31ede39adf256551d7e0c4ef7a2e
George Niculae <george.niculae20@yahoo.com>
2024-01-13 02:31:16 +0200

    Update

--- car-rental-request-validator/src/test/java/com/carrental/requestvalidator/service/SwaggerRequestValidatorServiceTest.java
+++ car-rental-request-validator/src/test/java/com/carrental/requestvalidator/service/SwaggerRequestValidatorServiceTest.java
@@ -1,7 +1,10 @@
 package com.carrental.requestvalidator.service;
 
+import com.carrental.requestvalidator.repository.SwaggerRepository;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
@@ -9,5 +12,13 @@ class SwaggerRequestValidatorServiceTest {
 
     @InjectMocks
     private SwaggerRequestValidatorService swaggerRequestValidatorService;
+
+    @Mock
+    private SwaggerRepository swaggerRepository;
+
+    @Test
+    void validateRequestTest_success() {
+
+    }
 
 }
