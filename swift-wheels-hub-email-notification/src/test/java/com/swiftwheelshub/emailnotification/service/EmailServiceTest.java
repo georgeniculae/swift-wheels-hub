c6c772f87f5cdde93941b06a9a5cc5ecbaa7f54b
George Niculae <george.niculae20@yahoo.com>
2024-02-05 23:34:14 +0200

    Update

--- swift-wheels-hub-email-notification/src/test/java/com/swiftwheelshub/emailnotification/service/EmailServiceTest.java
+++ swift-wheels-hub-email-notification/src/test/java/com/swiftwheelshub/emailnotification/service/EmailServiceTest.java
@@ -1,9 +1,13 @@
 package com.swiftwheelshub.emailnotification.service;
 
+import com.github.mustachejava.Mustache;
+import com.github.mustachejava.MustacheFactory;
 import com.sendgrid.Request;
 import com.sendgrid.Response;
 import com.sendgrid.SendGrid;
 import com.sendgrid.helpers.mail.Mail;
+import com.swiftwheelshub.dto.InvoiceResponse;
+import com.swiftwheelshub.emailnotification.util.TestUtils;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -11,9 +15,13 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.io.IOException;
+import java.io.StringWriter;
+import java.io.Writer;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -25,6 +33,15 @@ class EmailServiceTest {
     @Mock
     private SendGrid sendGrid;
 
+    @Mock
+    private MustacheFactory mustacheFactory;
+
+    @Mock
+    private Mustache mustache;
+
+    @Mock
+    private Writer writer;
+
     @Test
     void sendEmailTest_success() throws IOException {
         Mail mail = new Mail();
@@ -33,6 +50,19 @@ class EmailServiceTest {
         when(sendGrid.api(any(Request.class))).thenReturn(response);
 
         assertDoesNotThrow(() -> emailService.sendEmail(mail));
+    }
+
+    @Test
+    void createMailTest_success() {
+        InvoiceResponse invoiceResponse =
+                TestUtils.getResourceAsJson("/data/InvoiceResponse.json", InvoiceResponse.class);
+
+        when(mustacheFactory.compile(anyString())).thenReturn(mustache);
+        when(mustache.execute(any(StringWriter.class), any(Object.class))).thenReturn(writer);
+
+        Mail mail = emailService.createMail("test@email.com", invoiceResponse);
+
+        assertNotNull(mail);
     }
 
 }
