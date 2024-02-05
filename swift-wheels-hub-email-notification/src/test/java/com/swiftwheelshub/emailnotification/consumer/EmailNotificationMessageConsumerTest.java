c047b54d939cd3c233d933715769d4fd0dcfeaac
George Niculae <george.niculae20@yahoo.com>
2024-02-05 23:27:08 +0200

    Update

---
+++ swift-wheels-hub-email-notification/src/test/java/com/swiftwheelshub/emailnotification/consumer/EmailNotificationMessageConsumerTest.java
@@ -0,0 +1,38 @@
+package com.swiftwheelshub.emailnotification.consumer;
+
+import com.swiftwheelshub.dto.InvoiceResponse;
+import com.swiftwheelshub.emailnotification.service.UserNotificationService;
+import com.swiftwheelshub.emailnotification.util.TestUtils;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.GenericMessage;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+
+@ExtendWith(MockitoExtension.class)
+class EmailNotificationMessageConsumerTest {
+
+    @InjectMocks
+    private EmailNotificationMessageConsumer emailNotificationMessageConsumer;
+
+    @Mock
+    private UserNotificationService userNotificationService;
+
+    @Test
+    void consumeInvoiceTest_success() {
+        InvoiceResponse invoiceResponse =
+                TestUtils.getResourceAsJson("/data/InvoiceResponse.json", InvoiceResponse.class);
+
+        Message<InvoiceResponse> message = new GenericMessage<>(invoiceResponse);
+
+        doNothing().when(userNotificationService).notifyCustomer(any(InvoiceResponse.class));
+
+        emailNotificationMessageConsumer.consumeInvoice().accept(message);
+    }
+
+}
