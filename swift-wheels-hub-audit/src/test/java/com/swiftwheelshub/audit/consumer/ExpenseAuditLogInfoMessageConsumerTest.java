f4048a99e19ba99ffa3fc32c1081be977375f3ff
George Niculae <george.niculae20@yahoo.com>
2024-02-05 23:37:23 +0200

    Update

---
+++ swift-wheels-hub-audit/src/test/java/com/swiftwheelshub/audit/consumer/ExpenseAuditLogInfoMessageConsumerTest.java
@@ -0,0 +1,13 @@
+package com.swiftwheelshub.audit.consumer;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class ExpenseAuditLogInfoMessageConsumerTest {
+
+    @InjectMocks
+    private ExpenseAuditLogInfoMessageConsumer expenseAuditLogInfoMessageConsumer;
+
+}
