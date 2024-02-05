fa211a680daf2457108910a07f0c643090a1175b
George Niculae <george.niculae20@yahoo.com>
2024-02-05 23:10:57 +0200

    Update

--- swift-wheels-hub-api-gateway/swift-wheels-hub-cloud-gateway/src/test/java/com/swiftwheelshub/cloudgateway/filter/global/RequestHeaderModifierFilterTest.java
+++ swift-wheels-hub-api-gateway/swift-wheels-hub-cloud-gateway/src/test/java/com/swiftwheelshub/cloudgateway/filter/global/RequestHeaderModifierFilterTest.java
@@ -12,9 +12,11 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.mock.http.server.reactive.MockServerHttpRequest;
 import org.springframework.mock.web.server.MockServerWebExchange;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
 import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
@@ -54,10 +56,14 @@ class RequestHeaderModifierFilterTest {
         Map<String, Object> headers = Map.of(HttpHeaders.AUTHORIZATION, "Bearer " + tokenValue);
         Map<String, Object> claims = Map.of("preferred_username", "user");
 
-        Jwt jwt = new Jwt(tokenValue, Instant.now(), Instant.now().plus(30, ChronoUnit.MINUTES), headers, claims);
+        Jwt jwt =
+                new Jwt(tokenValue, Instant.now(), Instant.now().plus(30, ChronoUnit.MINUTES), headers, claims);
+        SimpleGrantedAuthority simpleGrantedAuthority = new SimpleGrantedAuthority("user");
 
         when(nimbusReactiveJwtDecoder.decode(anyString())).thenReturn(Mono.just(jwt));
         when(jwtAuthenticationTokenConverter.extractUsername(any(Jwt.class))).thenReturn("user");
+        when(jwtAuthenticationTokenConverter.extractGrantedAuthorities(any(Jwt.class)))
+                .thenReturn(Flux.just(simpleGrantedAuthority));
         when(chain.filter(any(ServerWebExchange.class))).thenReturn(Mono.empty());
 
         requestHeaderModifierFilter.filter(exchange, chain)
@@ -92,10 +98,15 @@ class RequestHeaderModifierFilterTest {
         Map<String, Object> headers = Map.of(HttpHeaders.AUTHORIZATION, "Bearer " + tokenValue);
         Map<String, Object> claims = Map.of("preferred_username", "user");
 
-        Jwt jwt = new Jwt(tokenValue, Instant.now(), Instant.now().plus(30, ChronoUnit.MINUTES), headers, claims);
+        Jwt jwt =
+                new Jwt(tokenValue, Instant.now(), Instant.now().plus(30, ChronoUnit.MINUTES), headers, claims);
+
+        SimpleGrantedAuthority simpleGrantedAuthority = new SimpleGrantedAuthority("user");
 
         when(nimbusReactiveJwtDecoder.decode(anyString())).thenReturn(Mono.just(jwt));
         when(jwtAuthenticationTokenConverter.extractUsername(any(Jwt.class))).thenReturn("user");
+        when(jwtAuthenticationTokenConverter.extractGrantedAuthorities(any(Jwt.class)))
+                .thenReturn(Flux.just(simpleGrantedAuthority));
         when(chain.filter(any(ServerWebExchange.class))).thenReturn(Mono.empty());
 
         requestHeaderModifierFilter.filter(exchange, chain)
