e4ee2e0f1dc7f96c8a9ffd750aefc0dbf8b4ade2
George Niculae <george.niculae20@yahoo.com>
2024-02-05 13:37:14 +0200

    Update

--- swift-wheels-hub-common/swift-wheels-hub-common-lib/src/main/java/com/swiftwheelshub/lib/util/HttpRequestUtil.java
+++ swift-wheels-hub-common/swift-wheels-hub-common-lib/src/main/java/com/swiftwheelshub/lib/util/HttpRequestUtil.java
@@ -3,7 +3,6 @@ package com.swiftwheelshub.lib.util;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.experimental.UtilityClass;
 import org.apache.commons.lang3.ObjectUtils;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 
@@ -18,19 +17,6 @@ public class HttpRequestUtil {
     private static final String X_API_KEY = "X-API-KEY";
     private static final String X_USERNAME = "X-USERNAME";
     private static final String X_ROLES = "X-ROLES";
-    private static final String HEADERS = "headers";
-
-    public static HttpEntity<String> getHttpEntity(HttpServletRequest request) {
-        final String authenticationHeader = extractAuthenticationToken(request);
-
-        HttpHeaders headers = new HttpHeaders();
-
-        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.set(X_API_KEY, authenticationHeader);
-
-        return new HttpEntity<>(HEADERS, headers);
-    }
 
     public static Consumer<HttpHeaders> mutateHeaders(HttpServletRequest request) {
         return httpHeaders -> {
@@ -39,18 +25,6 @@ public class HttpRequestUtil {
             httpHeaders.add(X_API_KEY, extractAuthenticationToken(request));
             httpHeaders.addAll(X_ROLES, extractRoles(request));
         };
-    }
-
-    public static HttpEntity<Object> getHttpEntityWithBody(HttpServletRequest request, Object o) {
-        final String authenticationHeader = extractAuthenticationToken(request);
-
-        HttpHeaders headers = new HttpHeaders();
-
-        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.set(X_API_KEY, authenticationHeader);
-
-        return new HttpEntity<>(o, headers);
     }
 
     public static String extractAuthenticationToken(HttpServletRequest request) {
