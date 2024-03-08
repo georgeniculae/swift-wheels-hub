084e4a3264c11b8d3353b9b48ec928630079c7fd
George <george.niculae20@yahoo.com>
2024-03-08 18:04:57 +0200

    Update

--- swift-wheels-hub-api-gateway/swift-wheels-hub-service-discovery/src/main/java/com/swiftwheelshub/servicediscovery/SwiftWheelsHubServiceDiscoveryApplication.java
+++ swift-wheels-hub-api-gateway/swift-wheels-hub-service-discovery/src/main/java/com/swiftwheelshub/servicediscovery/SwiftWheelsHubServiceDiscoveryApplication.java
@@ -1,4 +1,4 @@
-package com.swiftwheelshub.eurekaservicediscovery;
+package com.swiftwheelshub.servicediscovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class SwiftWheelsHubEurekaServiceDiscoveryApplication {
+public class SwiftWheelsHubServiceDiscoveryApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SwiftWheelsHubEurekaServiceDiscoveryApplication.class, args);
+        SpringApplication.run(SwiftWheelsHubServiceDiscoveryApplication.class, args);
     }
 
 }
--- swift-wheels-hub-api-gateway/swift-wheels-hub-service-discovery/src/main/java/com/swiftwheelshub/servicediscovery/config/EurekaSecurityConfig.java
+++ swift-wheels-hub-api-gateway/swift-wheels-hub-service-discovery/src/main/java/com/swiftwheelshub/servicediscovery/config/EurekaSecurityConfig.java
@@ -1,4 +1,4 @@
-package com.swiftwheelshub.eurekaservicediscovery.config;
+package com.swiftwheelshub.servicediscovery.config;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
