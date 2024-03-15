c2cbb8a23e1d5debd501f6359efd7d1430bf36e5
GeorgeNiculae <george.niculae20@yahoo.com>
2024-03-15 14:09:57 +0200

    Update

--- swift-wheels-hub-agency/src/main/java/com/swiftwheelshub/agency/service/CarService.java
+++ swift-wheels-hub-agency/src/main/java/com/swiftwheelshub/agency/service/CarService.java
@@ -33,6 +33,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Objects;
@@ -219,7 +220,7 @@ public class CarService {
             cars.add(generateCar(values));
         }
 
-        return cars;
+        return Collections.unmodifiableList(cars);
     }
 
     private Optional<PictureData> getPictureData(Sheet sheet, Cell cell) {
