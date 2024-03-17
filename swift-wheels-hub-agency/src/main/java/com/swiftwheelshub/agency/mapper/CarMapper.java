4aab1b52e921c310fbc06c807689e1e6855fbda1
George <george.niculae20@yahoo.com>
2024-03-17 20:37:22 +0200

    Update

--- swift-wheels-hub-agency/src/main/java/com/swiftwheelshub/agency/mapper/CarMapper.java
+++ swift-wheels-hub-agency/src/main/java/com/swiftwheelshub/agency/mapper/CarMapper.java
@@ -3,19 +3,16 @@ package com.swiftwheelshub.agency.mapper;
 import com.swiftwheelshub.dto.CarRequest;
 import com.swiftwheelshub.dto.CarResponse;
 import com.swiftwheelshub.entity.Car;
-import com.swiftwheelshub.exception.SwiftWheelsHubException;
-import org.apache.commons.lang3.ObjectUtils;
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-
-@Mapper(componentModel = "spring",
+@Mapper(
+        componentModel = "spring",
         unmappedTargetPolicy = ReportingPolicy.IGNORE,
-        injectionStrategy = InjectionStrategy.CONSTRUCTOR)
+        injectionStrategy = InjectionStrategy.CONSTRUCTOR
+)
 public interface CarMapper {
 
     @Mapping(target = "originalBranchId", expression = "java(car.getOriginalBranch().getId())")
@@ -27,17 +24,5 @@ public interface CarMapper {
     @Mapping(target = "bodyType", source = "bodyCategory")
     @Mapping(target = "carStatus", source = "carState")
     Car mapDtoToEntity(CarRequest carRequest);
-
-    default byte[] mapToImage(MultipartFile multipartFile) {
-        try {
-            if (ObjectUtils.isEmpty(multipartFile)) {
-                return null;
-            }
-
-            return multipartFile.getBytes();
-        } catch (IOException e) {
-            throw new SwiftWheelsHubException(e);
-        }
-    }
 
 }
