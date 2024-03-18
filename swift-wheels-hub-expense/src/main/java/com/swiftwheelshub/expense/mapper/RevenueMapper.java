0aa1479dd284d24a6d6f4e4caccfb989e8ba0c19
George <george.niculae20@yahoo.com>
2024-03-18 10:15:35 +0200

    Update

--- swift-wheels-hub-expense/src/main/java/com/swiftwheelshub/expense/mapper/RevenueMapper.java
+++ swift-wheels-hub-expense/src/main/java/com/swiftwheelshub/expense/mapper/RevenueMapper.java
@@ -6,9 +6,11 @@ import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = "spring",
+@Mapper(
+        componentModel = "spring",
         unmappedTargetPolicy = ReportingPolicy.IGNORE,
-        injectionStrategy = InjectionStrategy.CONSTRUCTOR)
+        injectionStrategy = InjectionStrategy.CONSTRUCTOR
+)
 public interface RevenueMapper {
 
     RevenueResponse mapEntityToDto(Revenue revenue);
