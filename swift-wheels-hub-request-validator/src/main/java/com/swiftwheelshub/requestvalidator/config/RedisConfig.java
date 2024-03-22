644682f49d048a282ffd3e5159d4eb1368d23522
George <george.niculae20@yahoo.com>
2024-03-22 19:46:06 +0200

    Update

--- swift-wheels-hub-request-validator/src/main/java/com/swiftwheelshub/requestvalidator/config/RedisConfig.java
+++ swift-wheels-hub-request-validator/src/main/java/com/swiftwheelshub/requestvalidator/config/RedisConfig.java
@@ -4,18 +4,14 @@ import com.swiftwheelshub.requestvalidator.model.SwaggerFile;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.GenericToStringSerializer;
 
 @Configuration
-@EnableRedisRepositories(basePackages = "com.swiftwheelshub.requestvalidator")
-@PropertySource("classpath:application.yml")
 public class RedisConfig {
 
     @Value("${spring.redis.host}")
