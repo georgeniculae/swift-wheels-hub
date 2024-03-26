ab63007f787a44a060f0d1e76c6afd05bbcc94a5
George <george.niculae20@yahoo.com>
2024-03-26 17:48:23 +0200

    Update

---
+++ swift-wheels-hub-agency/src/test/java/com/swiftwheelshub/agency/repository/CarRepositoryTest.java
@@ -0,0 +1,41 @@
+package com.swiftwheelshub.agency.repository;
+
+import com.swiftwheelshub.entity.Car;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Testcontainers
+@SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class CarRepositoryTest {
+
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16-alpine");
+
+    @Autowired
+    private CarRepository carRepository;
+
+    @Test
+    void checkIfConnectionEstablished() {
+        assertTrue(postgres.isCreated());
+    }
+
+    @Test
+    void findAllCarsTest_success() {
+        List<Car> cars = carRepository.findAll();
+        assertFalse(cars.isEmpty());
+    }
+
+}
