a012517fdb42fca659ee8f72700ec5b4d0fa5457
George <george.niculae20@yahoo.com>
2024-03-28 11:38:31 +0200

    Update

--- swift-wheels-hub-request-validator/src/main/java/com/swiftwheelshub/requestvalidator/service/SwaggerExtractorService.java
+++ swift-wheels-hub-request-validator/src/main/java/com/swiftwheelshub/requestvalidator/service/SwaggerExtractorService.java
@@ -8,7 +8,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.util.Pair;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 
