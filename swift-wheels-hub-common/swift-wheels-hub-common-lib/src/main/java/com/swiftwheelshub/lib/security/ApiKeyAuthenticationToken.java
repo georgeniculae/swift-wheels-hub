da790f46a3c30c8d081e7aea3ff458a9a1e01001
George <george.niculae20@yahoo.com>
2024-04-06 02:24:23 +0300

    Update

--- swift-wheels-hub-common/swift-wheels-hub-common-lib/src/main/java/com/swiftwheelshub/lib/security/ApiKeyAuthenticationToken.java
+++ swift-wheels-hub-common/swift-wheels-hub-common-lib/src/main/java/com/swiftwheelshub/lib/security/ApiKeyAuthenticationToken.java
@@ -1,23 +1,23 @@
 package com.swiftwheelshub.lib.security;
 
 import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.Transient;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
-import java.util.List;
+import java.util.Collection;
 
 @Transient
 public final class ApiKeyAuthenticationToken extends AbstractAuthenticationToken {
 
     private final String apiKey;
 
-    public ApiKeyAuthenticationToken(List<SimpleGrantedAuthority> grantedAuthorities, String apiKey) {
+    public ApiKeyAuthenticationToken(Collection<? extends GrantedAuthority> grantedAuthorities, String apiKey) {
         super(grantedAuthorities);
         super.setAuthenticated(false);
         this.apiKey = apiKey;
     }
 
-    public ApiKeyAuthenticationToken(List<SimpleGrantedAuthority> grantedAuthorities, String apiKey, boolean isAuthenticated) {
+    public ApiKeyAuthenticationToken(Collection<? extends GrantedAuthority> grantedAuthorities, String apiKey, boolean isAuthenticated) {
         super(grantedAuthorities);
         super.setAuthenticated(isAuthenticated);
         this.apiKey = apiKey;
