65156d2203bf6f4c3d1be3d6cca9ad5a7f945ea8
George <george.niculae20@yahoo.com>
2024-04-08 12:11:50 +0300

    Update

--- swift-wheels-hub-common/swift-wheels-hub-common-lib/src/main/java/com/swiftwheelshub/lib/security/SecurityConfig.java
+++ swift-wheels-hub-common/swift-wheels-hub-common-lib/src/main/java/com/swiftwheelshub/lib/security/SecurityConfig.java
@@ -14,7 +14,7 @@ import org.springframework.security.web.authentication.AnonymousAuthenticationFi
 
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity
+@EnableMethodSecurity(securedEnabled = true)
 @RequiredArgsConstructor
 @ConditionalOnBean(ApiKeyAuthenticationProvider.class)
 public class SecurityConfig {
