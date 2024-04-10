26dd09e4e667b8dd51d9cffa86c6ca3b237e6f88
George <george.niculae20@yahoo.com>
2024-04-10 18:27:39 +0300

    Update

--- swift-wheels-hub-api-gateway/src/main/java/com/swiftwheelshub/apigateway/SwiftWheelsHubApiGatewayApplication.java
+++ swift-wheels-hub-api-gateway/src/main/java/com/swiftwheelshub/apigateway/SwiftWheelsHubApiGatewayApplication.java
@@ -3,12 +3,14 @@ package com.swiftwheelshub.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import reactor.blockhound.BlockHound;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class SwiftWheelsHubApiGatewayApplication {
 
     public static void main(String[] args) {
+        BlockHound.install();
         SpringApplication.run(SwiftWheelsHubApiGatewayApplication.class, args);
     }
 
