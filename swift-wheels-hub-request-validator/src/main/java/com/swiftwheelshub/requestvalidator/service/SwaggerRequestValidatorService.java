84e60e3e0c54a82abcdb80b429e3de35ec9a38ec
George <george.niculae20@yahoo.com>
2024-07-07 03:01:48 +0300

    Update

--- swift-wheels-hub-request-validator/src/main/java/com/swiftwheelshub/requestvalidator/service/SwaggerRequestValidatorService.java
+++ swift-wheels-hub-request-validator/src/main/java/com/swiftwheelshub/requestvalidator/service/SwaggerRequestValidatorService.java
@@ -14,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Component
@@ -40,11 +41,13 @@ public class SwaggerRequestValidatorService {
     private SimpleRequest getSimpleRequest(IncomingRequestDetails request) {
         SimpleRequest.Builder simpleRequestBuilder = new SimpleRequest.Builder(request.method(), request.path());
 
-        request.headers()
-                .forEach(simpleRequestBuilder::withHeader);
+        for (Map.Entry<String, String> entry : request.headers().entrySet()) {
+            simpleRequestBuilder.withHeader(entry.getKey(), entry.getValue());
+        }
 
-        request.queryParams()
-                .forEach(simpleRequestBuilder::withQueryParam);
+        for (Map.Entry<String, String> entry : request.queryParams().entrySet()) {
+            simpleRequestBuilder.withQueryParam(entry.getKey(), entry.getValue());
+        }
 
         simpleRequestBuilder.withBody(request.body());
 
