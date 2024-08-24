bc3a515a26a6a2539b7492aed8cb71af08a76ff5
George <george.niculae20@yahoo.com>
2024-08-24 19:24:38 +0300

    Update

--- swift-wheels-hub-common/swift-wheels-hub-common-lib/src/main/java/com/swiftwheelshub/lib/config/cors/CorsConfigurer.java
+++ swift-wheels-hub-common/swift-wheels-hub-common-lib/src/main/java/com/swiftwheelshub/lib/config/cors/CorsConfigurer.java
@@ -1,14 +1,14 @@
 package com.swiftwheelshub.lib.config.cors;
 
-import org.springframework.context.annotation.Configuration;
 import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
+@Component
 @EnableWebMvc
-public class CorsConfig implements WebMvcConfigurer {
+public class CorsConfigurer implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(@NonNull CorsRegistry registry) {
