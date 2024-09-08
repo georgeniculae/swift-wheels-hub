bc6d249250451bfb009f108a2e3ed296fb023f1d
George <george.niculae20@yahoo.com>
2024-09-08 19:06:46 +0300

    Update

--- swift-wheels-hub-common/swift-wheels-hub-common-lib/src/main/java/com/swiftwheelshub/lib/config/restclient/RestClientConfig.java
+++ swift-wheels-hub-common/swift-wheels-hub-common-lib/src/main/java/com/swiftwheelshub/lib/config/restclient/RestClientConfig.java
@@ -4,7 +4,11 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
+
+import java.time.Duration;
 
 @Configuration
 public class RestClientConfig {
@@ -17,7 +21,17 @@ public class RestClientConfig {
 
     @Bean
     public RestClient restClient(@Qualifier("loadBalancedRestClientBuilder") RestClient.Builder restClientBuilder) {
-        return restClientBuilder.build();
+        return restClientBuilder.requestFactory(getClientHttpRequestFactory())
+                .build();
+    }
+
+    private ClientHttpRequestFactory getClientHttpRequestFactory() {
+        HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+
+        clientHttpRequestFactory.setConnectTimeout(Duration.ofSeconds(20));
+        clientHttpRequestFactory.setConnectionRequestTimeout(Duration.ofSeconds(15));
+
+        return clientHttpRequestFactory;
     }
 
 }
