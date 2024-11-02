44242ba6e54cfe7bca427d748d97ced5c028810b
George <george.niculae20@yahoo.com>
2024-11-02 13:53:56 +0200

    Update

---
+++ swift-wheels-hub-expense/src/main/java/com/swiftwheelshub/expense/scheduler/FailedInvoiceScheduler.java
@@ -0,0 +1,15 @@
+package com.swiftwheelshub.expense.scheduler;
+
+import com.swiftwheelshub.expense.repository.InvoiceRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class FailedInvoiceScheduler {
+
+    private final InvoiceRepository invoiceRepository;
+
+}
