4b68a4b54c6efc1467029cc58ff1ce0a67292066
George <george.niculae20@yahoo.com>
2024-11-12 23:46:11 +0200

    Update

--- swift-wheels-hub-expense/src/test/java/com/swiftwheelshub/expense/scheduler/FailedInvoiceSchedulerTest.java
+++ swift-wheels-hub-expense/src/test/java/com/swiftwheelshub/expense/scheduler/FailedInvoiceSchedulerTest.java
@@ -2,6 +2,7 @@ package com.swiftwheelshub.expense.scheduler;
 
 import com.swiftwheelshub.dto.AuthenticationInfo;
 import com.swiftwheelshub.dto.BookingClosingDetails;
+import com.swiftwheelshub.dto.BookingRollbackResponse;
 import com.swiftwheelshub.dto.BookingUpdateResponse;
 import com.swiftwheelshub.dto.CarUpdateDetails;
 import com.swiftwheelshub.dto.StatusUpdateResponse;
@@ -85,6 +86,31 @@ class FailedInvoiceSchedulerTest {
         assertDoesNotThrow(() -> failedInvoiceScheduler.processFailedInvoices());
 
         verify(executorService).invokeAll(anyList());
+    }
+
+    @Test
+    void processFailedInvoicesTest_failedCarUpdate_bookingRollbackSuccessful() {
+        Invoice failedInvoice = TestUtil.getResourceAsJson("/data/FailedInvoice.json", Invoice.class);
+
+        BookingUpdateResponse bookingUpdateResponse =
+                TestUtil.getResourceAsJson("/data/SuccessfulBookingUpdateResponse.json", BookingUpdateResponse.class);
+
+        StatusUpdateResponse statusUpdateResponse =
+                TestUtil.getResourceAsJson("/data/FailedStatusUpdateResponse.json", StatusUpdateResponse.class);
+
+        BookingRollbackResponse bookingRollbackResponse =
+                TestUtil.getResourceAsJson("/data/SuccessfulBookingRollbackResponse.json", BookingRollbackResponse.class);
+
+        when(invoiceRepository.findAllFailedInvoices()).thenReturn(List.of(failedInvoice));
+        when(failedBookingRollbackRepository.doesNotExistByBookingId(anyLong())).thenReturn(true);
+        when(bookingService.closeBooking(any(AuthenticationInfo.class), any(BookingClosingDetails.class)))
+                .thenReturn(bookingUpdateResponse);
+        when(carService.markCarAsAvailable(any(AuthenticationInfo.class), any(CarUpdateDetails.class)))
+                .thenReturn(statusUpdateResponse);
+        when(bookingService.rollbackBooking(any(AuthenticationInfo.class), anyLong()))
+                .thenReturn(bookingRollbackResponse);
+
+        assertDoesNotThrow(() -> failedInvoiceScheduler.processFailedInvoices());
     }
 
 }
