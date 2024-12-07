174f7013f1694129ff80a51ff5cdd10be7467f53
George <george.niculae20@yahoo.com>
2024-12-07 23:50:37 +0200

    Update

--- swift-wheels-hub-ai/src/main/java/com/swiftwheelshub/ai/config/ChatLanguageModelConfig.java
+++ swift-wheels-hub-ai/src/main/java/com/swiftwheelshub/ai/config/ChatLanguageModelConfig.java
@@ -1,22 +1,14 @@
 package com.swiftwheelshub.ai.config;
 
-import io.micrometer.observation.ObservationRegistry;
-import jakarta.persistence.criteria.Order;
 import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.model.ChatModel;
-import org.springframework.ai.model.function.FunctionCallback;
-import org.springframework.ai.model.function.FunctionCallbackContext;
 import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.ai.ollama.api.OllamaApi;
 import org.springframework.ai.ollama.api.OllamaOptions;
-import org.springframework.ai.ollama.management.ModelManagementOptions;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.time.Duration;
-import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
@@ -28,25 +20,18 @@ public class ChatLanguageModelConfig {
         return new OllamaApi(baseUrl);
     }
 
-//    @Bean
-//    public ChatModel chatModel(OllamaApi ollamaApi, ChatProperties chatProperties) {
-//        return new OllamaChatModel(
-//                ollamaApi,
-//                OllamaOptions.builder()
-//                        .withModel(chatProperties.getModel())
-//                        .withTemperature(chatProperties.getTemperature())
-//                        .build(),
-//                new FunctionCallbackContext(),
-//                List.of(
-//                        FunctionCallback.builder()
-//                                .function("suggestCar", _ -> "")
-//                                .inputType(Order.class)
-//                                .build()
-//                ),
-//                ObservationRegistry.create(),
-//                ModelManagementOptions.builder().withTimeout(Duration.ofSeconds(30)).build()
-//        );
-//    }
+    @Bean
+    public ChatModel chatModel(OllamaApi ollamaApi, ChatProperties chatProperties) {
+        return OllamaChatModel.builder()
+                .withOllamaApi(ollamaApi)
+                .withDefaultOptions(
+                        OllamaOptions.builder()
+                                .withModel(chatProperties.getModel())
+                                .withTemperature(chatProperties.getTemperature())
+                                .build()
+                )
+                .build();
+    }
 
     @Bean
     public ChatClient chatClient(ChatClient.Builder chatClientBuilder) {
