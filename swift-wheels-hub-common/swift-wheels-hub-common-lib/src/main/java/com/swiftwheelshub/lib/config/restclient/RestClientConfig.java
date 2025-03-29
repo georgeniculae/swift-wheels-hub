a36b594863c5c2e2a8bf8418f52038b6e786a2fe
George Niculae <george.niculae20@yahoo.com>
2025-03-29 21:15:09 +0200

    Update

--- swift-wheels-hub-common/swift-wheels-hub-common-lib/src/main/java/com/swiftwheelshub/lib/config/restclient/RestClientConfig.java
+++ swift-wheels-hub-common/swift-wheels-hub-common-lib/src/main/java/com/swiftwheelshub/lib/config/restclient/RestClientConfig.java
@@ -5,7 +5,7 @@ import org.springframework.boot.http.client.ClientHttpRequestFactoryBuilder;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
 import java.time.Duration;
@@ -17,7 +17,7 @@ public class RestClientConfig {
     @Bean(name = "loadBalancedRestClientBuilder")
     @LoadBalanced
     public RestClient.Builder restClientBuilder() {
-        SimpleClientHttpRequestFactory clientHttpRequestFactory = ClientHttpRequestFactoryBuilder.simple()
+        HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = ClientHttpRequestFactoryBuilder.httpComponents()
                 .withCustomizers(
                         List.of(
                                 requestFactory -> requestFactory.setConnectTimeout(Duration.ofSeconds(60)),
