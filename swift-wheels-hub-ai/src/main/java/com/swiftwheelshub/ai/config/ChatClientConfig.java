6216fd0913eb7e22e903249e511f3f55fa1f3188
George Niculae <george.niculae20@yahoo.com>
2025-04-27 20:09:28 +0300

    Update

--- swift-wheels-hub-ai/src/main/java/com/swiftwheelshub/ai/config/ChatClientConfig.java
+++ swift-wheels-hub-ai/src/main/java/com/swiftwheelshub/ai/config/ChatClientConfig.java
@@ -2,33 +2,17 @@ package com.swiftwheelshub.ai.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.model.ChatModel;
-import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.ai.ollama.api.OllamaApi;
-import org.springframework.ai.ollama.api.OllamaOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @RequiredArgsConstructor
-public class ChatLanguageModelConfig {
+public class ChatClientConfig {
 
     @Bean
     public OllamaApi ollamaApi() {
         return new OllamaApi();
-    }
-
-    @Bean
-    public ChatModel chatModel(OllamaApi ollamaApi, ChatProperties chatProperties) {
-        return OllamaChatModel.builder()
-                .ollamaApi(ollamaApi)
-                .defaultOptions(
-                        OllamaOptions.builder()
-                                .model(chatProperties.getModel())
-                                .temperature(chatProperties.getTemperature())
-                                .build()
-                )
-                .build();
     }
 
     @Bean
